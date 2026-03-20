621e6c6a781a6e341b4aa9e9a2f22c555661665b
Seu Nome <roquew890@gmail.com>
2026-03-20 20:11:43 -0300

    FIXED JAE

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
+
+// garantir que a pasta uploads existe
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+}
 
 // liberar imagens
 app.use("/uploads", express.static("uploads"));
@@ -14,7 +21,7 @@ app.use("/uploads", express.static("uploads"));
 const pessoasRoutes = require("./routes/pessoas");
 app.use("/pessoas", pessoasRoutes);
 
-// porta (IMPORTANTE pro Railway)
+// porta (Render / Railway)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
